503dbe91d4dd338743bc5483955e9543cf0f4dd3
kdegrave <kdegraveDS@gmail.com>
2019-08-17 14:51:01 -0600

    commit

--- submit_form.php
+++ submit_form.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
     $email_to = "degravek@gmail.com";
     $email_subject = "Email subject";
-    $first_name = $_POST['first_name']; // required
+    $first_name = $_POST['name']; // required
     $email_from = $_POST['email']; // required
     $telephone = $_POST['message']; // not required
 
@@ -12,7 +12,7 @@ if(isset($_POST['email'])) {
     }
 
     $email_message = "Form details below.\n\n";
-    $email_message .= "Name: ".clean_string($first_name)."\n";
+    $email_message .= "Name: ".clean_string($name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
     $email_message .= "Message: ".clean_string($message)."\n";
 
